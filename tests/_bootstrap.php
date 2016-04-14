07c6f1b7f3b4c0250d52d3b25162182abfa31d3e
Andrii Vasyliev <sol@hiqdev.com>
2016-04-14 07:53:08 +0000

    inited empty tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * HiQDev.com
+ *
+ * @link      http://hiqdev.github.io/
+ * @package   hiqdev.github.io
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+require_once __DIR__ . '/../vendor/autoload.php';
